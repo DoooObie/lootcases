d9ea2a472052c06fe4ecdbf956e8472c2583b4a6
fusion-develops <145299854+fusion-develops@users.noreply.github.com>
2024-06-09 13:36:18 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,43 +1,58 @@
-serverSetup = false
-CaseList = CFG.CaseList
-rarityWeights = CFG.RarityChance
-caseItemsWon = {}
+local rarityWeights = CFG.RarityChance
+local caseItemsWon = {}
 
-openCase = function(serverid, caseitems)
+local function getRandomIndex(totalWeight, caseitems)
+    local randomChance = math.random(1, totalWeight)
+    local cumulativeWeight = 0
+    local items = caseitems.Items
+    local count = #items
 
+    for i = 1, #count do
+        local item = items[i].rarity
+        cumulativeWeight += rarityWeights[item]
+        if randomChance <= cumulativeWeight then
+            return i
+        end
+    end
+
+    return count
+end
+
+local function LogCrateOpening(source, caseRarity, itemAmount, itemLabel)
+    return PerformHttpRequest(S_CFG.OpeningWebhook, function() end, 'POST', json.encode({username = "ALW Loot Crates", embeds = {
+        {
+            color = 1,
+            title = 'ALW Loot Crates',
+            description = GetPlayerName(source).." Just Opened A "..caseRarity.." Crate And Received x"..itemAmount.." ".. itemLabel,
+            footer = {
+                text = 'ALW Loot Crates',
+                icon_url = 'https://i.imgur.com/ecmVFwa.png',
+            },
+        }
+    }}), { ['Content-Type'] = 'application/json' })
+end
+
+local function OpenCase(serverid, caseitems)
     local totalWeight = 0
 
     for _, item in ipairs(caseitems.Items) do
-        totalWeight = totalWeight + rarityWeights[item.rarity]
+        totalWeight += rarityWeights[item.rarity]
     end
 
-    local function getRandomIndex()
-        local randomChance = math.random(1, totalWeight)
-        local cumulativeWeight = 0
-
-        for i, item in ipairs(caseitems.Items) do
-            cumulativeWeight = cumulativeWeight + rarityWeights[item.rarity]
-            if randomChance <= cumulativeWeight then
-                return i
-            end
-        end
-
-        return #caseitems.Items
-    end
-
-    local randomIndex = getRandomIndex()
-    local randomItem = caseitems.Items[randomIndex]
+    local randomIndex <const> = getRandomIndex(totalWeight, caseitems)
+    local randomItem <const> = caseitems.Items[randomIndex]
     TriggerClientEvent('gg_lootcases:openCase', serverid, randomItem, caseitems)
 
     if CFG.RarityChance[randomItem.rarity] < 10 then 
         if randomItem.item == 'money' then
-            CFG.ChatNotify(("^3%s^7 won ^3$%s^7 from a ^3%s^7 case!"):format(GetPlayerName(serverid), GroupDigits(randomItem.amount), randomItem.rarity))
+            CFG.ChatNotify("^3".. GetPlayerName(serverid).. "^7 won ^3$".. GroupDigits(randomItem.amount).."^7 from a ^3".. randomItem.rarity.."^7 case!")
+
             LogCrateOpening(serverid, randomItem.rarity, GroupDigits(randomItem.amount), 'Money')
-        elseif string.match(string.lower(randomItem.item), 'weapon_') then
-            CFG.ChatNotify(("^3%s^7 won a ^3%s^7 from a ^3%s^7 case!"):format(GetPlayerName(serverid), GetWeaponLabel(randomItem.item), randomItem.rarity))
+        elseif randomItem.item:lower():find('WEAPON_') then
+            CFG.ChatNotify("^3"..GetPlayerName(serverid).."^7 won a ^3"..GetWeaponLabel(randomItem.item).."^7 from a ^3"..randomItem.rarity.."^7 case!")
             LogCrateOpening(serverid, randomItem.rarity, 1, GetWeaponLabel(randomItem.item))
         else
-            CFG.ChatNotify(("^3%s^7 won x%s ^3%s^7 from a ^3%s^7 case!"):format(GetPlayerName(serverid), randomItem.amount, GetItemLabel(randomItem.item), randomItem.rarity))
+            CFG.ChatNotify("^3"..GetPlayerName(serverid).."^7 won x"..randomItem.amount.." ^3".. GetItemLabel(randomItem.item).."^7 from a ^3"..randomItem.rarity.."^7 case!")
             LogCrateOpening(serverid, randomItem.rarity, randomItem.amount, GetItemLabel(randomItem.item))
         end
     end
@@ -46,34 +61,14 @@ openCase = function(serverid, caseitems)
 end
 
 
-function LogCrateOpening(source, caseRarity, itemAmount, itemLabel)
-    local playername = GetPlayerName(source)
-    for k,v in ipairs(GetPlayerIdentifiers(source)) do
-        if string.sub(v, 1, string.len("license:")) == "license:" then
-            license = v
-        end
-    end
 
-    local embed = {
-        {
-            ["color"] = 1,
-            ["title"] = 'ALW Loot Crates',
-            -- ["description"] = "Player: **"..playername.."** Just used a cloned card | "..license.." **"..text.."**",
-            ["description"] = ("%s Just Opened A %s Crate And Received x%s %s"):format(playername, caseRarity, itemAmount, itemLabel),
-            ["footer"] = {
-                ["text"] = 'ALW Loot Crates',
-                ["icon_url"] = 'https://i.imgur.com/ecmVFwa.png',
-            },
-        }
-    }
-    PerformHttpRequest(S_CFG.OpeningWebhook, function(err, text, headers) end, 'POST', json.encode({username = "ALW Loot Crates", embeds = embed}), { ['Content-Type'] = 'application/json' })
-end
 
 
 RegisterNetEvent('gg_lootcases:rewardPlayer', function()
-    if caseItemsWon[source] then
-        rewardPlayer(source, caseItemsWon[source])
-        caseItemsWon[source] = nil
+    local won = caseItemsWon[source]
+    if won then
+        OpenCase(source, won)
+        won = nil
     else
         print('gg_lootcases:rewardPlayer - No item found for source: ' .. source)
     end
